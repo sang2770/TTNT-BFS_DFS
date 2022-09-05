dd13ddff3c436990111b36af8f877a770605a21b
sang.nv1 <sang.nv1@evotek.vn>
2022-09-05 09:32:32 +0700

    fix parent

--- src/BFS.py
+++ src/BFS.py
@@ -1,3 +1,4 @@
+from pickle import TRUE
 from tkinter import Grid
 from tabulate import tabulate
 
@@ -5,72 +6,72 @@ class BFS:
     visited = []  # List for visited nodes.
     queue = []  # Initialize a queue
     road = []
-    parents = [] # List for parent node
-    table = [] # List for table
+    parents = {}
+    table = []
+    node = 0
+    target = 0
 
-    # Read File
-    def readFile(self, filename):
+    def read_file(self, filename):
         graph = {}
+    
         f = open(filename)
-        firstRow = f.readline()
-        node,target = firstRow.split() 
+        first_row = f.readline()
+        self.node, self.target = first_row.split()
 
         rows = f.readlines()
         for row in rows:
             if row != '\n': 
                 row = row.split()
                 graph[row.pop(0)] = row or ''
-        return graph, node, target
+        return graph, self.node, self.target
 
 
-    def bfs(self, graph, node, target):  # function for BFS
+    def bfs(self, graph, node, target):  # function for DFS
         self.visited.append(node)
         self.queue.append(node)
 
         while self.queue:          # Creating loop to visit each node
             m = self.queue.pop(0)
-            self.parents.append(m)
-
             if m in graph.keys():
                 for neighbour in graph[m]:
                     if neighbour not in self.visited:
                         self.visited.append(neighbour)
+                        self.parents[neighbour] = m
                         self.queue.append(neighbour)
-
                 row = [m, "TTKT" if m == target else ", ".join(graph[m]), ", ".join(self.visited.copy()), ", ".join(self.queue.copy())]
                 self.table.append(row)
-
+                
             else:
                 print("Invalid input")
                 break
 
             if m == target:
-                parent = m
-                self.road.append(parent)
-                for p in reversed(self.parents):
-                    if parent in graph[p]:
-                        parent = p
-                        self.road.append(parent)
+                self.getRoad()
                 return self.table
         return False
+    def getRoad(self):
+        while(self.node != self.target):
+            self.road.append(self.target)
+            self.target = self.parents[self.target]
+        self.road.append(self.node)
 
-    def createTable(self, table):
-        print("------------- BFS -------------")
-        head = ["Trạng Thái Đầu", "Danh Sách Kề", "Danh sách đi qua", "Hàng đợi"]
+    def create_table(self, table):
+        print("------------- DFS -------------")
+        head = ["Trạng Thái Đầu", "Danh Sách Kề", "Danh sách đi qua", "Ngăn xếp"]
 
         print(tabulate(table, headers=head, tablefmt="grid"))
 
     def test(self, file_name):
         try:
-            graph, node, target = self.read_file(file_name)
+            self.graph, self.node, self.target= self.read_file(file_name)
         except:
             print("File không hợp lệ")
             return
-        table = self.bfs(graph, node, target)
+        table = self.bfs(self.graph, self.node, self.target)
         if table:
             self.create_table(table)
         self.road.reverse()
         print("Đường đi: "," -> ".join(self.road))
 
-bfs = BFS()
-bfs.test("resource/input_BDFS.txt")
+dfs = BFS()
+dfs.test("resource/input_BDFS.txt")
--- src/DFS.py
+++ src/DFS.py
@@ -1,3 +1,4 @@
+from pickle import TRUE
 from tkinter import Grid
 from tabulate import tabulate
 
@@ -5,22 +6,24 @@ class DFS:
     visited = []  # List for visited nodes.
     stack = []  # Initialize a stack
     road = []
-    parents = []
+    parents = {}
     table = []
+    node = 0
+    target = 0
 
     def read_file(self, filename):
         graph = {}
     
         f = open(filename)
         first_row = f.readline()
-        node,target = first_row.split()
+        self.node, self.target = first_row.split()
 
         rows = f.readlines()
         for row in rows:
             if row != '\n': 
                 row = row.split()
                 graph[row.pop(0)] = row or ''
-        return graph, node, target
+        return graph, self.node, self.target
 
 
     def dfs(self, graph, node, target):  # function for DFS
@@ -29,14 +32,12 @@ class DFS:
 
         while self.stack:          # Creating loop to visit each node
             m = self.stack.pop(0)
-            self.parents.append(m)
-
             if m in graph.keys():
                 for neighbour in graph[m]:
                     if neighbour not in self.visited:
                         self.visited.append(neighbour)
+                        self.parents[neighbour] = m
                         self.stack.insert(0,neighbour)
-
                 row = [m, "TTKT" if m == target else ", ".join(graph[m]), ", ".join(self.visited.copy()), ", ".join(self.stack.copy())]
                 self.table.append(row)
                 
@@ -45,14 +46,14 @@ class DFS:
                 break
 
             if m == target:
-                parent = m
-                self.road.append(parent)
-                for p in reversed(self.parents):
-                    if parent in graph[p]:
-                        parent = p
-                        self.road.append(parent)
+                self.getRoad()
                 return self.table
         return False
+    def getRoad(self):
+        while(self.node != self.target):
+            self.road.append(self.target)
+            self.target = self.parents[self.target]
+        self.road.append(self.node)
 
     def create_table(self, table):
         print("------------- DFS -------------")
@@ -62,11 +63,11 @@ class DFS:
 
     def test(self, file_name):
         try:
-            graph, node, target = self.read_file(file_name)
+            self.graph, self.node, self.target= self.read_file(file_name)
         except:
             print("File không hợp lệ")
             return
-        table = self.dfs(graph, node, target)
+        table = self.dfs(self.graph, self.node, self.target)
         if table:
             self.create_table(table)
         self.road.reverse()
